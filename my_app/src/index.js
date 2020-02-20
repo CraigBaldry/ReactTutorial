0408d0958fec35d78a57392206b59df6704348d9
CraigBaldry <smojoe86@gmail.com>
2020-02-20 22:16:08 +0800

    Making an interactive component

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -22,10 +22,20 @@ class ShoppingList extends React.Component {
 // ========================================
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
             </button>
         );
     }
